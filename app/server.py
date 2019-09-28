652f8b4025345ed7b0567975b6f93dee40a7028c
Sergio R <srg00017@gmail.com>
2019-09-28 17:21:27 +0200

    Added new model with opt field in learn

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1pF63MwiLDECzOWPmKGwVAOTEkl_G7dYd'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1--i0YjWPaYwDD2ioEQF2Smzjc_KyCIOy'
 export_file_name = 'export.pkl'
 
 classes = ['quiet', 'fire']
